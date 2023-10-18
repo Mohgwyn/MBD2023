6c655cabbe71e790cc2a65e42c540a03db5154eb
Mohgwyn <fjgarciacobo@gmail.com>
2023-10-18 19:25:20 +0200

    Final solution with records implemented in the iterface

--- src/main/java/org/uma/mbd/mdDegree/degree/Degree.java
+++ src/main/java/org/uma/mbd/mdDegree/degree/Degree.java
@@ -1,13 +1,31 @@
 package org.uma.mbd.mdDegree.degree;
 
+record Fahrenheit(double degree) implements Degree{}
+record Celsius(double degree) implements Degree{}
+
 public sealed interface Degree permits Celsius, Fahrenheit{
-    boolean isFrozen();
+    default boolean isFrozen() {
+        return switch(this) {
+            case Celsius(var x) -> x < 0;
+            case Fahrenheit(var x) -> x <32;
+        };
+    };
     default Degree toCelsius() {
-        return this;
+        return switch(this) {
+            case Celsius(var x) -> this;
+            case Fahrenheit(var x) -> Degree.fahrenheit((x-32)/1.8);
+        };
+
     }
     default Degree toFahrenheit() {
-        return this;
+        return switch(this) {
+            case Celsius(var x) -> Degree.celsius(x*1.8+32);
+            case Fahrenheit(var x) -> this;
+        };
     }
+
+
+    // Métodos factoría
     static Degree fahrenheit(double degree) {
         return new Fahrenheit(degree);
     }
