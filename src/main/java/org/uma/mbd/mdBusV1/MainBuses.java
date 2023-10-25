3681aae99da9fd3c22a83d4eb140a1686c68a187
Mohgwyn <fjgarciacobo@gmail.com>
2023-10-25 18:12:18 +0200

    Added recursos folder to gitignore

--- src/main/java/org/uma/mbd/mdBusV1/MainBuses.java
+++ src/main/java/org/uma/mbd/mdBusV1/MainBuses.java
@@ -1,4 +1,4 @@
-package org.uma.mbd.mdBusV1;
+package org.uma.mbd.mdBusV1L;
 
 import org.uma.mbd.mdBusV1.buses.Criterio;
 import org.uma.mbd.mdBusV1.buses.EnMatricula;
@@ -13,12 +13,12 @@ public class MainBuses {
         Servicio servicio = new Servicio("Malaga");
         try {
         	System.out.println(servicio.getCiudad());
-            servicio.leeBuses("src\\main\\java\\org\\uma\\mbd\\mdBusV1\\buses.txt");
+            servicio.leeBuses("recursos/mdBusV1/buses.txt");
             Criterio cr1 = new PorLinea(21);
-            servicio.guarda("src\\main\\java\\org\\uma\\mbd\\mdBusV1\\linea21.txt", cr1);
+            servicio.guarda("recursos/mdBusV1/linea21.txt", cr1);
             servicio.guarda(new PrintWriter(System.out,true), cr1);
             Criterio cr2 = new EnMatricula("29");
-            servicio.guarda("src\\main\\java\\org\\uma\\mbd\\mdBusV1\\contiene29.txt", cr2);
+            servicio.guarda("recursos/mdBusV1/contiene29.txt", cr2);
             servicio.guarda(new PrintWriter(System.out,true), cr2);
         } catch (IOException e) {
             System.err.println("No existe el fichero de entrada");
