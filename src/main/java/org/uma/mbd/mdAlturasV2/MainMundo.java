9d2b8e63732ea4b34747ff750ddcb3c13669add4
Mohgwyn <fjgarciacobo@gmail.com>
2023-10-27 21:25:06 +0200

    Added TreeSets with required orders

--- src/main/java/org/uma/mbd/mdAlturasV2/MainMundo.java
+++ src/main/java/org/uma/mbd/mdAlturasV2/MainMundo.java
@@ -1,12 +1,12 @@
 package org.uma.mbd.mdAlturasV2;
 
-import org.uma.mbd.mdAlturasV2.alturas.EnContinente;
 import org.uma.mbd.mdAlturasV2.alturas.MenoresQue;
 import org.uma.mbd.mdAlturasV2.alturas.Mundo;
 import org.uma.mbd.mdAlturasV2.alturas.Pais;
 
 import java.io.IOException;
 import java.util.Comparator;
+import java.util.Set;
 import java.util.TreeSet;
 
 public class MainMundo {
@@ -22,14 +22,18 @@ public class MainMundo {
 
         Mundo paises = new Mundo();
         paises.leePaises("recursos/mdAlturas/alturas.txt");
+        Set<Pais> paisesAltura = new TreeSet<>(OrdenAltura);
+        Set<Pais> paisesAlfabetico = new TreeSet<>(OrdenAlfabetico);
+        Set<Pais> paisesContAlfa = new TreeSet<>(OrdenContinente.thenComparing(OrdenAlfabetico));
+        Set<Pais> paisesContAlfaInv = new TreeSet<>(OrdenContinente.thenComparing(OrdenAlfabetico.reversed()));
+        Set<Pais> paisesContNatural = new TreeSet<>(OrdenContinente.thenComparing(Comparator.naturalOrder()));
 
         for (Pais pais : paises.selecciona(new MenoresQue(1.70))) {
-            System.out.println(pais);
+            paisesAltura.add(pais);
+            paisesAlfabetico.add(pais);
+            paisesContAlfa.add(pais);
+            paisesContAlfaInv.add(pais);
+            paisesContNatural.add(pais);
         }
-        System.out.println();
-        for (Pais pais : paises.selecciona(new EnContinente("Europe"))) {
-            System.out.println(pais);
-        }
-
     }
 }
