e9048a3563cece780f4260165f61b412f9875768
Mohgwyn <fjgarciacobo@gmail.com>
2023-11-01 20:04:10 +0100

    Created DHont class

---
+++ src/main/java/org/uma/mbd/mdPartidos/partidos/DHont.java
@@ -0,0 +1,27 @@
+package org.uma.mbd.mdPartidos.partidos;
+
+import java.util.List;
+import java.util.Map;
+
+public class DHont extends DHontSimple implements CriterioSeleccion{
+
+    private double minPor;
+
+    public DHont(double minPor) {
+        super();
+        if (minPor < 0 || minPor >= 15) {
+            throw new ExceptionElecciones("Minimo porcentaje invalido.");
+        }
+        this.minPor = minPor;
+    }
+    @Override
+    public Map<Partido, Integer> ejecuta(List<Partido> partidos, int numEsc) {
+        int votos = partidos.stream()
+                .mapToInt(Partido::getVotos)
+                .sum();
+        List<Partido> partidosFiltrados = partidos.stream()
+                .filter(p -> ((double)p.getVotos()/votos)*100 > minPor)
+                .toList();
+        return super.ejecuta(partidosFiltrados, numEsc);
+    }
+}
