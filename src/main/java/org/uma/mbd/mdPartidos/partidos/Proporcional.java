6e99de75aa6eaeb087b3a5f0d084f42fe2652789
Mohgwyn <fjgarciacobo@gmail.com>
2023-11-01 20:04:26 +0100

    Created Proporcional class

---
+++ src/main/java/org/uma/mbd/mdPartidos/partidos/Proporcional.java
@@ -0,0 +1,29 @@
+package org.uma.mbd.mdPartidos.partidos;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Proporcional implements CriterioSeleccion{
+
+    @Override
+    public Map<Partido, Integer> ejecuta(List<Partido> partidos, int numEsc) {
+        Set<Token> tks = creaTokens(partidos, numEsc);
+        tks = Token.seleccioneTokens(tks, numEsc);
+        return Token.generaResultados(tks);
+    }
+    private Set<Token> creaTokens(List<Partido> partidos, int numEsc) {
+        int votos = partidos.stream()
+                .mapToInt(Partido::getVotos)
+                .sum();
+        double vpe = (double)votos/numEsc;
+        Set<Token> tks = new TreeSet<>();
+        for (Partido p : partidos) {
+            for (int i=0; i<numEsc; i++) {
+                tks.add(new Token(p, (double)p.getVotos()-(vpe*i)));
+            }
+        }
+        return tks;
+    }
+}
